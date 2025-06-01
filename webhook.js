18df685a7af4c832519e9eab89c07987e557be00
0xMaxime <maximemasquelier@mailo.fr>
2025-06-01 10:21:18 +0200

    fix .env

--- webhook.js
+++
@@ -1,5 +0,0 @@
-app.post("/api/cotisation-update", async (req, res) => {
-  const { Membres_Id } = req.body.record;   // ID du membre concerné
-  await handleExpiration(Membres_Id, client);
-  res.sendStatus(204);
-});
